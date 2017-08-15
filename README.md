1a6e521693fe6a41bde013f646d39ae7712e6c78
Anmol Sethi <anmol@aubble.com>
2017-08-15 14:37:36 -0400

    README.md and bug fixes

---
+++ README.md
@@ -0,0 +1,22 @@
+# sshocks5
+
+## Usage
+
+```zsh
+sshocks5 --help
+```
+
+## Explanation
+
+sshocks5 will use `ssh` to connect to a host of your choosing
+and open a SOCKS5 proxy on localhost:5030 (configurable). Then, it will
+use `networksetup` to modify your `Wi-Fi` (configurable) network
+to make use of the proxy. Once you press Ctrl+C in the terminal, it will
+stop your network from using SOCKS5 Proxy and then kill the SSH process.
+
+sshocks5 will use `sudo` to modify the network configuration.
+
+## TODO
+
+- [ ] Only run `sudo` once instead of once for setting the proxy
+      and another for removing it.
--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"flag"
+	"net"
 	"os"
 	"os/exec"
 	"os/signal"
+	"syscall"
 
 	"github.com/nhooyr/log"
+	"github.com/pkg/errors"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatalf("usage: %v <host>", os.Args[0])
+func waitSSH(cmd *exec.Cmd, done chan struct{}) error {
+	defer close(done)
+	err := cmd.Wait()
+	pErr, ok := err.(*exec.ExitError)
+	if !ok {
+		return errors.Wrap(err, "unexpected error waiting for ssh")
 	}
-	ssh := exec.Command("ssh", os.Args[1], "-D localhost:5030", "-o ControlPath=none", "-N")
+	status := pErr.ProcessState.Sys().(syscall.WaitStatus)
+	if status.Signal() != os.Kill {
+		return errors.Wrap(err, "ssh not killed by us")
+	}
+	return nil
+}
+
+func main() {
+	host := flag.String("host", "", "host to connect to")
+	socks5Addr := flag.String("D", "localhost:5030", "socks5 listening address (addr:port)")
+	network := flag.String("net", "Wi-Fi", "network to configure to use SOCKS5 proxy")
+	flag.Parse()
+
+	log := log.Logger{
+		Out: log.LineWriter{
+			Out: &log.AtomicWriter{Out: os.Stderr},
+		},
+	}
+
+	if *host == "" {
+		log.Print("please provide a host")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	ssh := exec.Command("ssh", *host, "-D", *socks5Addr, "-o", "ControlPath=none", "-N")
 	err := ssh.Start()
 	if err != nil {
 		log.Fatalf("failed to start ssh: %v", err)
@@ -20,18 +52,30 @@ func main() {
 
 	sshDone := make(chan struct{})
 	go func() {
-		defer close(sshDone)
-		err := ssh.Wait()
+		err := waitSSH(ssh, sshDone)
 		if err != nil {
-			log.Printf("failed to wait for ssh: %v", err)
+			log.Printf("error waiting for ssh: %v", err)
 		}
 	}()
 
-	networksetup := exec.Command("networksetup", "-setsocksfirewallproxy", "Wi-Fi", "localhost", "5030")
+	socks5Host, socks5Port, err := net.SplitHostPort(*socks5Addr)
+	if err != nil {
+		log.Fatalf("failed to split host port socks5Addr: %v", err)
+	}
+	if socks5Host == "" || socks5Host == "*" {
+		socks5Host = "localhost"
+	}
+
+	networksetup := exec.Command("sudo", "networksetup", "-setsocksfirewallproxy", *network, socks5Host, socks5Port)
+	networksetup.Stdin = os.Stdin
+	networksetup.Stdout = os.Stdout
+	networksetup.Stderr = os.Stderr
 	err = networksetup.Run()
 	if err != nil {
 		log.Fatalf("failed to set socks5 proxy: %v", err)
 	}
+
+	log.Print("initialized")
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
@@ -43,10 +87,13 @@ func main() {
 			log.Printf("erorr killing ssh: %v", err)
 		}
 	case <-sshDone:
-		log.Print("ssh unexpectedly quit, state: %v", ssh.ProcessState)
+		log.Printf("ssh unexpectedly quit, state: %v", ssh.ProcessState)
 	}
 
-	networksetup = exec.Command("networksetup", "-setsocksfirewallproxystate", "Wi-Fi", "off")
+	networksetup = exec.Command("sudo", "networksetup", "-setsocksfirewallproxystate", *network, "off")
+	networksetup.Stdin = os.Stdin
+	networksetup.Stdout = os.Stdout
+	networksetup.Stderr = os.Stderr
 	err = networksetup.Run()
 	if err != nil {
 		log.Fatalf("failed to turn off socks5 proxy: %v", err)
